ea396685e4c110da5c3df3825045400bbfcb5e81
MarioVydra <134620325+MarioVydra@users.noreply.github.com>
2024-02-21 15:31:08 +0100

    Added Leibniz Method and Laplace Expansion

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main() {
     std::string fileName, dataType;
     int size, method, variant;
+    laplaceVariant laplace;
 
     std::cout << "Enter the name of the file in the .txt format:" << std::endl;
     while (!(std::cin >> fileName) || (fileName.length() < 4 || fileName.substr(fileName.length() - 4, fileName.length()) != ".txt")) {
@@ -24,6 +25,21 @@ int main() {
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 
+    if (method == 3) {
+        std::cout << "Choose a variant of Laplace Expansion [ '1' - Full Laplace Expansion, '2' - Laplace Expansion + Rule of Sarrus, '3' - Laplace Expansion + Gauss Elimination ]" << std::endl;
+        while (!(std::cin >> variant) || (variant != 1 && variant != 2 && variant != 3)) {
+            std::cout << "Invalid input. Please enter valid number of the variant of Laplace Expansion." << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        switch (variant) {
+            case 1: laplace = laplaceVariant::FULL_LAPLACE_EXPANSION; break;
+            case 2: laplace = laplaceVariant::LAPLACE_RULE_OF_SARRUS; break;
+            case 3: laplace = laplaceVariant::LAPLACE_GAUSS_ELIMINATION; break;
+            default: std::cerr << "Invalid variant of Laplace Expansion." << std::endl; return -1;
+        }
+    }
+
     if (dataType == "float") {
         Matrix<float> matrix(size);
         fileHandler.fillMatrix(matrix);
@@ -33,20 +49,8 @@ int main() {
         } else if (method == 2) {
             alg.leibnizMethod(matrix, true);
         } else {
-            std::cout << "Choose a variant of Laplace Expansion [ '1' - Full Laplace Expansion, '2' - Laplace Expansion + Rule of Sarrus, '3' - Laplace Expansion + Gauss Elimination ]" << std::endl;
-            while (!(std::cin >> variant) || (method != 1 && method != 2 && method != 3)) {
-                std::cout << "Invalid input. Please enter valid number of the variant of Laplace Expansion." << std::endl;
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            }
             std::cout << std::endl;
-            if (variant == 1) {
-                alg.laplaceMethod(matrix, laplaceVariant::FULL_LAPLACE_EXPANSION);
-            } else if (variant == 2) {
-                alg.laplaceMethod(matrix, laplaceVariant::LAPLACE_RULE_OF_SARRUS);
-            } else {
-                alg.laplaceMethod(matrix, laplaceVariant::LAPLACE_GAUSS_ELIMINATION);
-            }
+            alg.laplaceMethod(matrix, laplace);
         }
     } else if (dataType == "double") {
         Matrix<double> matrix(size);
@@ -57,24 +61,12 @@ int main() {
         } else if (method == 2) {
             alg.leibnizMethod(matrix, true);
         } else {
-            std::cout << "Choose a variant of Laplace Expansion [ '1' - Full Laplace Expansion, '2' - Laplace Expansion + Rule of Sarrus, '3' - Laplace Expansion + Gauss Elimination ]" << std::endl;
-            while (!(std::cin >> variant) || (method != 1 && method != 2 && method != 3)) {
-                std::cout << "Invalid input. Please enter valid number of the variant of Laplace Expansion." << std::endl;
-                std::cin.clear();
-                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            }
             std::cout << std::endl;
-            if (variant == 1) {
-                alg.laplaceMethod(matrix, laplaceVariant::FULL_LAPLACE_EXPANSION);
-            } else if (variant == 2) {
-                alg.laplaceMethod(matrix, laplaceVariant::LAPLACE_RULE_OF_SARRUS);
-            } else {
-                alg.laplaceMethod(matrix, laplaceVariant::LAPLACE_GAUSS_ELIMINATION);
-            }
+            alg.laplaceMethod(matrix, laplace);
         }
     } else {
         std::cerr << "Invalid data type of the matrix.";
-        return 1;
+        return -1;
     }
     return 0;
 }
