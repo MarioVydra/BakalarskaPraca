7fbef7f075c57d9267c4d49656c6f6946624d3a8
MarioVydra <134620325+MarioVydra@users.noreply.github.com>
2024-04-13 22:18:24 +0200

    Added comments to the code

---
+++ cmake-build-release/CMakeFiles/BakalarskaPraca.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BakalarskaPraca.exe"
+  "BakalarskaPraca.exe.manifest"
+  "BakalarskaPraca.lib"
+  "BakalarskaPraca.pdb"
+  "CMakeFiles/BakalarskaPraca.dir/main.cpp.obj"
+  "CMakeFiles/BakalarskaPraca.dir/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BakalarskaPraca.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
